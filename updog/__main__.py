6f0ce788033a6acb42d591348fc5861f9d2df45c
Henry Prince <henry@prince.co>
2025-11-16 14:53:48 -0500

    Add --cors flag for Cross-Origin Resource Sharing
    Closes #54
    
    Add --cors argument to enable CORS headers, useful for accessing
    updog from web applications during security testing and development.
    
    Dependencies:
    - Add flask-cors ^6.0.1 via Poetry
    
    Usage:
      updog --cors  # Enable CORS for all origins

--- updog/__main__.py
+++ updog/__main__.py
@@ -4,6 +4,7 @@ import argparse
 
 from flask import Flask, render_template, send_file, redirect, request, send_from_directory, url_for, abort
 from flask_httpauth import HTTPBasicAuth
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.serving import run_simple
@@ -35,6 +36,7 @@ def parse_arguments():
                         help='Port to serve [Default=9090]')
     parser.add_argument('--password', type=str, default='', help='Use a password to access the page. (No username)')
     parser.add_argument('--ssl', action='store_true', help='Use an encrypted connection')
+    parser.add_argument('--cors', action='store_true', help='Enable CORS (Cross-Origin Resource Sharing)')
     parser.add_argument('--version', action='version', version='%(prog)s v'+VERSION)
 
     args = parser.parse_args()
@@ -50,6 +52,9 @@ def main():
 
     app = Flask(__name__)
     auth = HTTPBasicAuth()
+
+    if args.cors:
+        CORS(app)
 
     global base_directory
     base_directory = args.directory
